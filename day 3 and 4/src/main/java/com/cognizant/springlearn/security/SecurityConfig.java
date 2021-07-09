4ba894bc6173cbd0da16a5f5e9c33e9437c19fac
rathun <65532258+rathun@users.noreply.github.com>
2021-07-09 18:57:46 +0530

    Update SecurityConfig.java

--- day 3 and 4/src/main/java/com/cognizant/springlearn/security/SecurityConfig.java
+++ day 3 and 4/src/main/java/com/cognizant/springlearn/security/SecurityConfig.java
@@ -29,7 +29,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.csrf().disable().httpBasic().and()
-				.authorizeRequests().antMatchers("/countries").hasRole("USER")
+				.authorizeRequests()
+				//.antMatchers("/countries").hasRole("USER")
 				.antMatchers("/authenticate").hasAnyRole("USER", "ADMIN").anyRequest()
 				.authenticated().and()
 				.addFilter(new JwtAuthorizationFilter(authenticationManager()));
